722b2102f2d0dce258f712d8a7201d0b383f2092
aashimodi14 <ash007modi@gmail.com>
2021-02-16 11:18:03 +0530

    initial commit - terraform

---
+++ terraform/rmq/main.tf
@@ -0,0 +1,16 @@
+provider "aws"{
+    region = "us-east-1"
+    profile=var.profile
+}
+
+resource "aws_instance" "rmq" {
+    ami = "ami-047a51fa27710816e"
+    instance_type = "t2.micro"
+    key_name = "rabbitmq"
+    vpc_security_group_ids = [ "sg-01b0a50a79dc4ba55" ]
+
+    tags = {
+        Name = var.name
+        group = var.group
+    }
+}
---
+++ terraform/rmq/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+    description = "Name of the instance to be deployed" 
+}
+
+variable "group" {
+    description = "Group tag for ansible to identify RMQ"
+}
+
+variable "profile" {
+    description = "profile we will use to deploy"
+}
